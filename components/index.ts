99602ea0bd27be20271e6520780dc59dd3c04d8f
qurrahazam <qurrahazam@gmail.com>
2026-01-01 14:00:02 +0500

    ADD: Create index.ts to export components for UI, layout, brand assets, landing page, and icons

---
+++ components/index.ts
@@ -0,0 +1,14 @@
+// UI Components
+export * from './ui';
+
+// Layout Components
+export * from './layout';
+
+// Common Components
+export * from './brand-assets';
+
+// Landing Page Components
+export * from './landing';
+
+// Icons
+export * from './icons';
