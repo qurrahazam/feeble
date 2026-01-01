95f9e39b7e078abefc9328a03c7edf344ba733d4
qurrahazam <qurrahazam@gmail.com>
2026-01-01 14:34:50 +0500

    ADD: Include EllipseBg and StillBirds components in HomePage layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import {
   CloudLayer,
   BirdsVector,
   BirdsIllustration,
+  EllipseBg,
+  StillBirds,
 } from '@/components/landing';
 
 export default function HomePage() {
   return (
     <main className="relative min-h-screen w-full overflow-hidden">
+      <EllipseBg />
       <HeroSection />
       <CloudLayer />
+      <StillBirds />
       <BirdsVector />
       <BirdsIllustration />
     </main>
