72531639b1949b6c24422a77948b1ffa2615ef7b
qurrahazam <qurrahazam@gmail.com>
2026-01-01 14:35:48 +0500

    ADD: Create EllipseBg component for bg

---
+++ components/landing/ellipse.tsx
@@ -0,0 +1,47 @@
+export function EllipseBg() {
+  return (
+    <div className="fixed top-0 left-0 pointer-events-none z-0">
+      <svg 
+        width="500" 
+        height="334" 
+        viewBox="0 0 500 334" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g filter="url(#filter0_f_1_146)">
+          <ellipse 
+            cx="143.159" 
+            cy="-7.8811" 
+            rx="235.633" 
+            ry="220.881" 
+            fill="white" 
+            fillOpacity="0.7"
+          />
+        </g>
+        <defs>
+          <filter 
+            id="filter0_f_1_146" 
+            x="-213.474" 
+            y="-349.762" 
+            width="713.265" 
+            height="683.762" 
+            filterUnits="userSpaceOnUse" 
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+            <feBlend 
+              mode="normal" 
+              in="SourceGraphic" 
+              in2="BackgroundImageFix" 
+              result="shape"
+            />
+            <feGaussianBlur 
+              stdDeviation="60.5" 
+              result="effect1_foregroundBlur_1_146"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
+}
