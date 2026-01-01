7ca8c6d8bb9b30c303a0ff7ca2ef68fde0145def
qurrahazam <qurrahazam@gmail.com>
2026-01-01 13:58:13 +0500

    ADD: Implement useWindowSize hook for responsive window size tracking

---
+++ hooks/use-window-size.ts
@@ -0,0 +1,30 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+interface WindowSize {
+  width: number;
+  height: number;
+}
+
+export function useWindowSize(): WindowSize {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    const updateSize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    updateSize();
+    window.addEventListener('resize', updateSize);
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  return windowSize;
+}
